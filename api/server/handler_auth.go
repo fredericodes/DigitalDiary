3ff4f83cddb846ed9d96073667f80c2bb906f762
fredericodes <jilsonfreddy@hotmail.com>
2022-03-16 20:24:10 +0530

    Add postgres db: Refactor account register & login

--- api/server/handler_auth.go
+++ api/server/handler_auth.go
@@ -13,69 +13,62 @@ import (
 
 func (srv *Server) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	srv.Logger.Info().Println(requestLogInfo(r, reqBody))
 
 	var form model.LoginForm
 	if err := json.Unmarshal(reqBody, &form); err != nil {
-		srv.Logger.Error().Println(err)
 
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errFormDecodingErr)))
 		return
 	}
 
-	if len(form.Email) == 0 || len(form.Password) == 0 {
+	if len(form.Username) == 0 || len(form.Password) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%v"}`, errInvalidCredentials)))
 		return
 	}
 
-	userKeyValue, err := srv.RedisDb.ReadUserExists(form.Email)
-	if err != nil {
-		srv.Logger.Error().Println(fmt.Sprintf(`%v %v`, form.Email, errUserEmailDoesntExistsErr))
+	tx := srv.DB.TxBegin()
+	user, _ := tx.ReadUserByUsername(form.Username)
+	if user == nil {
+		tx.Rollback()
 
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errUserEmailDoesntExistsErr)))
 		return
 	}
 
-	var user model.User
-	jsonUser := []byte(userKeyValue)
-	if err := json.Unmarshal(jsonUser, &user); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errServerProcessingErr)))
-		return
-	}
-
 	match, err := auth.ComparePassword(form.Password, user.Password)
 	if !match || err != nil {
+		tx.Rollback()
+
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%v"}`, errInvalidCredentials)))
 		return
-	} else {
-		token, err := GenerateToken(user.Id)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Error generating JWT token: " + err.Error()))
-			return
-		} else {
-			w.Header().Set("Authorization", "Bearer "+token)
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(fmt.Sprintf(`{"token": "%v"}`, token)))
-			return
-		}
 	}
 
+	token, err := GenerateToken(user.Id)
+	if err != nil {
+		tx.Rollback()
+
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error generating JWT token: " + err.Error()))
+		return
+	}
+
+	tx.Commit()
+
+	w.Header().Set("Authorization", "Bearer "+token)
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`{"token": "%v"}`, token)))
+	return
 }
 
 func (srv *Server) HandleRegister(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	srv.Logger.Info().Println(requestLogInfo(r, reqBody))
 
 	var form model.RegisterForm
 	if err := json.Unmarshal(reqBody, &form); err != nil {
-		srv.Logger.Error().Println(err)
 
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errFormDecodingErr)))
@@ -85,41 +78,34 @@ func (srv *Server) HandleRegister(w http.ResponseWriter, r *http.Request) {
 	passwordConfigs := configs.ReadPasswordEnvConfigs()
 	hash, err := auth.GeneratePassword(passwordConfigs, form.Password)
 	if err != nil {
-		srv.Logger.Error().Println(err)
 
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errPasswordHashGenFailure)))
 		return
 	}
 
+	tx := srv.DB.TxBegin()
+
 	user := form.ToModel(hash)
-	jsonData, err := json.Marshal(user)
-	if err != nil {
-		srv.Logger.Error().Println(err)
-
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errServerProcessingErr)))
-		return
-	}
-
-	_, err = srv.RedisDb.ReadUserExists(form.Email)
-	if err == nil {
-		srv.Logger.Error().Println(fmt.Sprintf(`%v %v`, form.Email, errUserEmailExistsErr))
+	userFromDb, _ := tx.ReadUserByUsername(form.Username)
+	if userFromDb != nil {
+		tx.Rollback()
 
 		w.WriteHeader(http.StatusConflict)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errUserEmailExistsErr)))
 		return
 	}
 
-	if err := srv.RedisDb.CreateUser(user.Email, jsonData); err != nil {
-		srv.Logger.Error().Println(err)
+	if err := tx.CreateUser(user); err != nil {
+		tx.Rollback()
 
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, errServerProcessingErr)))
 		return
 	}
 
-	srv.Logger.Info().Println(fmt.Sprintf(`"created user": "%v"`, string(jsonData)))
+	tx.Commit()
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf(`{"created": "%s"}`, string(jsonData))))
+	w.Write([]byte(fmt.Sprintf(`{"created": "%s"}`, user.Username)))
 }

b3a882603b71f9dfe0ca58dd86dc4655afdc6d73
fredericodes <jilsonfreddy@hotmail.com>
2022-03-16 23:39:01 +0530

    Update API and UI codebase backed by postgres db

--- api/go.mod
+++ api/go.mod
@@ -1,4 +1,4 @@
-module github.com/FreddyJilson/diarynote
+module api
 
 go 1.17
 
--- api/migration/scripts/20200922140000_create_initial_tables.sql
+++ api/migration/scripts/20200922140000_create_initial_tables.sql
@@ -9,6 +9,16 @@ CREATE TABLE IF NOT EXISTS users
     CONSTRAINT uix_users_username UNIQUE (username)
 );
 
+CREATE TABLE IF NOT EXISTS journals
+(
+    id           UUID                      NOT NULL,
+    user_id      UUID                      NOT NULL,
+    entry_date   VARCHAR(255)              NOT NULL,
+    content      TEXT                      NULL,
+    PRIMARY KEY (id)
+);
+
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS journals;
